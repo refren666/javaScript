53b857672c0019e1a873675b10b1eb049ae81c0a
refren666 <denys86shvaiber@gmail.com>
2021-12-18 20:12:13 +0200

    42

--- lesson14/homework14.js
+++ lesson14/homework14.js
@@ -6,151 +6,151 @@
 
 // Callbacks:
 
-// const startOfADay = (isDayOff, callback) => {
-//     setTimeout(() => {
-//         if (!isDayOff) {
-//             callback(null, 'Waking up...'); // Error first, data last!
-//         } else {
-//             callback('Sleep whole day...', null);
-//         }
-//     }, 1000)
-// }
-//
-// const brushTeeth = (isToothpaste, callback) => {
-//     setTimeout(() => {
-//         if (isToothpaste) {
-//             callback(null, 'Brushing teeth...');
-//         } else {
-//             callback('No toothpaste...', null);
-//         }
-//     }, 800)
-// }
-//
-// const eatFood = (isFood, callback) => {
-//     setTimeout(() => {
-//         if (isFood) {
-//             callback(null, 'Eating food...');
-//         } else {
-//             callback('Need to buy some food...', null);
-//         }
-//     }, 1200)
-// }
-//
-// const dressUp = (isClothes, callback) => {
-//     setTimeout(() => {
-//         if (isClothes) {
-//             callback(null, 'Dressing up...');
-//         } else {
-//             callback('Nothing to wear...', null);
-//         }
-//     }, 700)
-// }
-//
-// const commuting = (isGoodWeather, isDisaster = false, callback) => {
-//     setTimeout(() => {
-//         if (isDisaster) {
-//             callback('Staying home...', null);
-//         } else if (isGoodWeather) {
-//             callback(null, 'Going to bus station...');
-//         } else if (!isGoodWeather) {
-//             callback(null, 'Taking taxi...');
-//         }
-//     }, 1200)
-// }
-//
-// const doWork = (callback) => {
-//     setTimeout(() => {
-//         callback('Working...');
-//     }, 2000)
-// }
-//
-// const goBackHome = (isWorkDone, callback) => {
-//     setTimeout(() => {
-//         if (isWorkDone) {
-//             callback(null, 'Going back home...');
-//         } else {
-//             callback('Continue working...', null);
-//         }
-//     }, 1300)
-// }
-//
-// const goSleep = (tired, callback) => {
-//     setTimeout(() => {
-//         if (tired) {
-//             callback(null, 'Going to sleep...');
-//         } else {
-//             callback('Watching films...', null);
-//         }
-//     }, 1400)
-//
-// }
-//
-// startOfADay(false, (error, start) => {
-//     if (error) {
-//         console.error(error);
-//     } else {
-//         console.log(start);
-//         brushTeeth(true, (error2, brush) => {
-//             if (error2) {
-//                 console.error(error2);
-//             } else {
-//                 console.log(brush);
-//                 eatFood(true, (error0, eat) => {
-//                     if (error0) {
-//                         console.error(error0);
-//                     } else {
-//                         console.log(eat);
-//                         dressUp(true, (error3, dressing) => {
-//                             if (error3) {
-//                                 console.error(error3);
-//                             } else {
-//                                 console.log(dressing);
-//                                 commuting(true, false, (error4, commute) => {
-//                                     if (error4) {
-//                                         console.error(error4);
-//                                     } else {
-//                                         console.log(commute);
-//                                         doWork(work => {
-//                                             console.log(work);
-//                                             goBackHome(true, (error5, goBack) => {
-//                                                 if (error5) {
-//                                                     console.error(error5);
-//                                                 } else {
-//                                                     console.log(goBack);
-//                                                     eatFood(true, (error6, eat) => {
-//                                                         if (error6) {
-//                                                             console.error(error6);
-//                                                         } else {
-//                                                             console.log(eat);
-//                                                             brushTeeth(true, (error7, brush) => {
-//                                                                 if (error7) {
-//                                                                     console.error(error7);
-//                                                                 } else {
-//                                                                     console.log(brush);
-//                                                                     goSleep(true, (error8, finish) => {
-//                                                                         if (error8) {
-//                                                                             console.error(error8);
-//                                                                         } else {
-//                                                                             console.log(finish);
-//                                                                         }
-//                                                                     })
-//                                                                 }
-//                                                             })
-//                                                         }
-//                                                     })
-//                                                 }
-//                                             })
-//                                         })
-//                                     }
-//                                 })
-//                             }
-//                         })
-//                     }
-//                 })
-//             }
-//         })
-//     }
-// })
+const startOfADay = (isDayOff, callback) => {
+    setTimeout(() => {
+        if (!isDayOff) {
+            callback(null, 'Waking up...'); // Error first, data last!
+        } else {
+            callback('Sleep whole day...', null);
+        }
+    }, 1000)
+}
+
+const brushTeeth = (isToothpaste, callback) => {
+    setTimeout(() => {
+        if (isToothpaste) {
+            callback(null, 'Brushing teeth...');
+        } else {
+            callback('No toothpaste...', null);
+        }
+    }, 800)
+}
+
+const eatFood = (isFood, callback) => {
+    setTimeout(() => {
+        if (isFood) {
+            callback(null, 'Eating food...');
+        } else {
+            callback('Need to buy some food...', null);
+        }
+    }, 1200)
+}
+
+const dressUp = (isClothes, callback) => {
+    setTimeout(() => {
+        if (isClothes) {
+            callback(null, 'Dressing up...');
+        } else {
+            callback('Nothing to wear...', null);
+        }
+    }, 700)
+}
+
+const commuting = (isGoodWeather, isDisaster = false, callback) => {
+    setTimeout(() => {
+        if (isDisaster) {
+            callback('Staying home...', null);
+        } else if (isGoodWeather) {
+            callback(null, 'Going to bus station...');
+        } else if (!isGoodWeather) {
+            callback(null, 'Taking taxi...');
+        }
+    }, 1200)
+}
+
+const doWork = (callback) => {
+    setTimeout(() => {
+        callback('Working...');
+    }, 2000)
+}
+
+const goBackHome = (isWorkDone, callback) => {
+    setTimeout(() => {
+        if (isWorkDone) {
+            callback(null, 'Going back home...');
+        } else {
+            callback('Continue working...', null);
+        }
+    }, 1300)
+}
+
+const goSleep = (tired, callback) => {
+    setTimeout(() => {
+        if (tired) {
+            callback(null, 'Going to sleep...');
+        } else {
+            callback('Watching films...', null);
+        }
+    }, 1400)
+
+}
+
+startOfADay(false, (error, start) => {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(start);
+        brushTeeth(true, (error2, brush) => {
+            if (error2) {
+                console.error(error2);
+            } else {
+                console.log(brush);
+                eatFood(true, (error3, eat) => {
+                    if (error3) {
+                        console.error(error3);
+                    } else {
+                        console.log(eat);
+                        dressUp(true, (error4, dressing) => {
+                            if (error4) {
+                                console.error(error4);
+                            } else {
+                                console.log(dressing);
+                                commuting(true, false, (error5, commute) => {
+                                    if (error5) {
+                                        console.error(error5);
+                                    } else {
+                                        console.log(commute);
+                                        doWork(work => {
+                                            console.log(work);
+                                            goBackHome(true, (error6, goBack) => {
+                                                if (error6) {
+                                                    console.error(error6);
+                                                } else {
+                                                    console.log(goBack);
+                                                    eatFood(true, (error7, eat) => {
+                                                        if (error7) {
+                                                            console.error(error7);
+                                                        } else {
+                                                            console.log(eat);
+                                                            brushTeeth(true, (error8, brush) => {
+                                                                if (error8) {
+                                                                    console.error(error8);
+                                                                } else {
+                                                                    console.log(brush);
+                                                                    goSleep(true, (error9, finish) => {
+                                                                        if (error9) {
+                                                                            console.error(error9);
+                                                                        } else {
+                                                                            console.log(finish);
+                                                                        }
+                                                                    })
+                                                                }
+                                                            })
+                                                        }
+                                                    })
+                                                }
+                                            })
+                                        })
+                                    }
+                                })
+                            }
+                        })
+                    }
+                })
+            }
+        })
+    }
+})
 
 // ****************************************************************************************************************
 
@@ -302,9 +302,9 @@ const goSleep = (tired) => {
 
 // async function routine() {
 //     try {
-//         const start = await startOfADay(false);
+//         const start = await startOfADay(false); // start = result from resolve/reject in startOfADay
 //         console.log(start);
-//         const brush = await brushTeeth(start);
+//         const brush = await brushTeeth(start); // brush = result from resolve/reject in brushTeeth...
 //         console.log(brush);
 //         const eat = await eatFood(true);
 //         console.log(eat);
@@ -328,4 +328,3 @@ const goSleep = (tired) => {
 // }
 //
 // routine();
-
